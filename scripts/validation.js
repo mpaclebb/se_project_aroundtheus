bd39c27672442be8858517953474c6503e5a53a3
Marku Pacleb <markupacleb@gmail.COM>
2024-11-21 12:59:12 -0500

    validation add

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,4 +1,24 @@
-enableValidation({
+function showInputError(formEl, inputEl, { inputErrorClass, errorClass }) {
+    const errorMessageEl = formEl.querySelector('#${inputEl.id}-error');
+    inputEl.classList.add(inputErrorClass);
+    errorMessageEl.textContent = inputEl.validationMessage;
+    errorMessageEl.classList.add(errorClass); 
+}
+
+function hideInputError(formEl, inputEl, { inputErrorClass, errorClass }) {
+    const errorMessageEl = formEl.querySelector('#${inputEl.id}-error');
+    inputEl.classList.add(inputErrorClass);
+    errorMessageEl.textContent = inputEl.validationMessage;
+    errorMessageEl.classList.add(errorClass);
+}
+
+function hideInputError(formEl, inputEl, { inputErrorClass, errorClass })
+function enableValidation(options){
+    const formEls = document.querySelectorAll(".popup__form");
+    formEls.forEach(formEl) => {
+        form
+    }
+
   formSelector: ".popup__form",
   inputSelector: ".popup__input",
   submitButtonSelector: ".popup__button",
